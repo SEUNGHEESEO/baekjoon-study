6bae3d50e23c0e5d61222e3358d8c22c78f04bc4
SEUNGHEESEO <86587656+SEUNGHEESEO@users.noreply.github.com>
2025-03-10 16:17:08 +0900

    [level 2] Title: 올바른 괄호, Time: 7.36 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,12 @@
 def solution(s):
-    answer = True
-    if s[0] == ")":
-        answer = False
-    else:
-        cnt = 0
-        for i in s:
-            if i == "(":
-                cnt += 1
-            elif i == ")": 
-                cnt -= 1
-            if cnt < 0: 
-                answer = False
-                break
-        if cnt != 0:
-            answer = False
-    return answer
+    stack = []
+    
+    for char in s:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if not stack:  # 스택이 비어있으면 짝이 안 맞음
+                return False
+            stack.pop()  # 짝을 이루면 열린 괄호 하나 꺼냄
+    
+    return len(stack) == 0  # 빈값이어야 짝이 모두 맞음
